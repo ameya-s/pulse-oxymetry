b286226528511224210cc09e491ce1fe7a7f4fa7
Ameya Sanjay Dahale <ameya.dahale@clarifai.com>
2020-06-27 17:57:54 +0300

    t2

--- test.py
+++ test.py
@@ -13,7 +13,8 @@ filter_dc_ir = []
 
 i = 0
 while True:
-    if i>5000: break
+    if i > 10000:
+        break
     try:
         mx30.read_sensor()
         print('i => ', i)
@@ -24,10 +25,23 @@ while True:
         data[i]['ir'] = mx30.ir
         data[i]['red'] = mx30.red
 
+        if i == 0:
+            data[i]['w_ir'] = data[i]['ir']
+            data[i]['result_ir'] = data[i]['w_ir']
+
+            data[i]['w_red'] = data[i]['red']
+            data[i]['result_red'] = data[i]['w_red']
+        else:
+            data[i]['w_ir'] = data[i]['ir'] + 0.95 * data[i-1]['w_ir']
+            data[i]['result_ir'] = data[i]['w_ir'] - data[i-1]['w_ir']
+
+            data[i]['w_red'] = data[i]['red'] + 0.95 * data[i - 1]['w_red']
+            data[i]['result_red'] = data[i]['w_red'] - data[i - 1]['w_red']
+
+
         i += 1
     except IOError:
         print('IO error : error in reading')
 
 with open('data_dump_spo2.json', 'w') as f:
     f.write(json.dumps(data))
-
