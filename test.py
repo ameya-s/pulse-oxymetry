981d6fb108c50aa20c6ecf7d757cfd23740b71df
Ameya Sanjay Dahale <ameya.dahale@clarifai.com>
2020-07-10 23:44:12 +0300

    t1

--- test.py
+++ test.py
@@ -16,6 +16,9 @@ def main():
     mx30.enable_spo2()
     data = {}
 
+    pts = 1800  # points used for peak finding (400 Hz, I recommend at least 4s (1600 pts)
+    t_vals, y_vals = [], []
+
     i = 0
     while True:
         if i > 5000:
@@ -56,13 +59,71 @@ def main():
             # if i>0:
             #     data[i]['butter_ir'] =
 
-            i += 1
+            while len(y_vals) < pts:
+                try:
+                    t_vals.append(float(data[i]['tst']) / 1000.0)
+                    y_vals.append(float(data[i]['red']))
+
+                    heart_rate(t_vals, y_vals)
+
+                    i += 1
+                except:
+                    continue
+
+            t_vals, y_vals = [], []
         except IOError:
             print('IO error : error in reading')
 
     with open('data_dump_spo2.json', 'w') as f:
         f.write(json.dumps(data))
 
+
+def heart_rate(t_vec, red_vec):
+    heart_rate_span = [10, 250]  # max span of heart rate
+
+    smoothing_size = 20  # convolution smoothing size
+
+    # convolve, calculate gradient, and remove bad endpoints
+    samp_rate = 1 / np.mean(np.diff(t_vec))  # average sample rate for determining peaks
+    min_time_bw_samps = (60.0 / heart_rate_span[1])
+
+    t_vals = t_vec
+    y_vals = red_vec
+
+    y_vals = np.convolve(y_vals, np.ones((smoothing_size,)), 'same') / smoothing_size
+    red_grad = np.gradient(y_vals, t_vals)
+    red_grad[0:int(smoothing_size / 2) + 1] = np.zeros((int(smoothing_size / 2) + 1,))
+    red_grad[-int(smoothing_size / 2) - 1:] = np.zeros((int(smoothing_size / 2) + 1,))
+
+    y_vals = np.append(np.repeat(y_vals[int(smoothing_size / 2)], int(smoothing_size / 2)),
+                       y_vals[int(smoothing_size / 2):-int(smoothing_size / 2)])
+    y_vals = np.append(y_vals, np.repeat(y_vals[-int(smoothing_size / 2)], int(smoothing_size / 2)))
+
+    # peak locator algorithm
+    peak_locs = np.where(red_grad < -np.std(red_grad))
+    # if len(peak_locs[0])==0:
+    #     continue
+
+    prev_pk = peak_locs[0][0]
+    true_peak_locs, pk_loc_span = [], []
+    for ii in peak_locs[0]:
+        y_pk = y_vals[ii]
+        if (t_vals[ii] - t_vals[prev_pk]) < min_time_bw_samps:
+            pk_loc_span.append(ii)
+        else:
+            if pk_loc_span == []:
+                true_peak_locs.append(ii)
+            else:
+                true_peak_locs.append(int(np.mean(pk_loc_span)))
+                pk_loc_span = []
+
+        prev_pk = int(ii)
+
+    t_peaks = [t_vals[kk] for kk in true_peak_locs]
+    # if t_peaks==[]:
+    #     continue
+    # else:
+    print('BPM: {0:2.1f}'.format(60.0 / np.mean(np.diff(t_peaks))))
 
 if __name__ == "__main__":
     main()
